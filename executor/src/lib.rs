cac1232d2ad6f0783dfddba599f615876ecfe6b9
chenguoma <jguo@sei.ecnu.edu.cn>
2026-04-23 15:24:01 +0800

    extrac action,implemented a task orchestration framework

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -1,6 +1,7 @@
 mod executor;
 mod pose;
 mod state;
+mod action;
 
 pub use crate::executor::Executor;
 pub use crate::pose::Pose;
--- executor/src/state.rs
+++ executor/src/state.rs
@@ -1,10 +1,64 @@
-#[derive(Default,Copy,Clone)]
+//#[derive(Default,Copy,Clone)]
+
+use super::action::Action;
+
 pub(crate) struct State{
     pub(crate) is_reverse: bool,
+}
+
+impl Default for State{
+    fn default() -> Self {
+        State{is_reverse:false}
+    }
 }
 
 impl State{
     pub(crate) fn be_reverse(&mut self){
         self.is_reverse=!self.is_reverse;
+    }
+
+    pub(crate)  fn assemble(&self, cmd:char) -> Vec<Action>{
+        match cmd {
+            'M' => self.move_assemble(),
+            'L' => self.turn_left_assemble(),
+            'R' => self.turn_right_assemble(),
+            _ => Vec::new(),
+        }
+    }
+
+    fn move_assemble(&self) ->Vec<Action> {
+        let mut actions=Vec::new();
+        let direction = if self.is_reverse {-1} else {1};
+        let action =Action::Forward(direction);
+
+        actions.push(action);
+
+        actions
+    }
+
+    fn turn_left_assemble(&self) ->Vec<Action>{
+        let mut actions=Vec::new();
+
+        let turn_action=if self.is_reverse{
+            Action::TurnRight
+        } else{
+            Action::TurnLeft
+        };
+        actions.push(turn_action);
+
+        actions
+    }
+
+    fn turn_right_assemble(&self) ->Vec<Action>{
+        let mut actions=Vec::new();
+
+        let turn_action=if self.is_reverse{
+            Action::TurnLeft
+        } else{
+            Action::TurnRight
+        };
+        actions.push(turn_action);
+
+        actions
     }
 }
